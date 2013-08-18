2700a59e97ad5b65796ef11c94305b3a9661b775
concise <concise_net@163.com>
2013-08-18 21:46:59 +0800

    0818night commit

--- servinfo/home/Lib/Action/IndexAction.class.php
+++ servinfo/home/Lib/Action/IndexAction.class.php
@@ -12,10 +12,40 @@ class IndexAction extends Action {
 		if(!session('uname')){
 			$this->error("还没有登录，或者会话已过期，请重新登录","__APP__/login");
 		}
-		$this->db=M('admin');
+		$this->assign('admin_name',session('uname'));
+		$this->db=M('servlist');
 	}
     public function index(){
-		$this->assign('admin_name',session('uname'));
+		$this->all_serv=$this->db->select();
+		//$this->assign('admin_name',session('uname'));
 		$this->display();
     }
+
+	//添加服务器
+	public function serv_add(){
+		if($_POST['sname']){
+			$data['sname']=trim(htmlspecialchars($_POST['sname']));
+			$data['ip']=trim(htmlspecialchars($_POST['ip']));
+			$re_arr=$this->db->where("sname='".$data['sname']."'")->find();
+			if(is_array($re_arr)) $this->error("该服务器名称已存在");
+			if($this->db->add($data)){
+				$info=$this->db->where("sname='".$data['sname']."'")->find();
+				$this->ajaxReturn($info,'info',1);
+			}
+		}
+	}
+
+	//填写巡检信息
+	public function fill_in(){
+		$this->display();
+	}
+	// 修改
+	public function edit(){
+		$this->display();
+	}
+
+	//删除
+	public function del(){
+		
+	}
 }
---
+++ servinfo/home/runtime/Cache/9fbfe03070fcd055fd8fc878671dd96f.php
@@ -0,0 +1,20 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>服务器信息修改——服务器巡检信息录入系统|德州之窗网</title>
+<link href="__PUBLIC__/css/default.css" type="text/css" rel="stylesheet"/>
+<script src="__PUBLIC__/js/jquery.js" type="text/javascript"></script>
+
+</head>
+<body>
+	<div class="header">
+		<div class="logo_top"><img src="__PUBLIC__/images/logo.jpg" height="77"></div>
+		<div class="top_info">欢迎你：<?php echo ($admin_name); ?>，<a href="/">[系统首页]</a>&nbsp;|&nbsp;<a href="__APP__/login/logout">[安全退出]</a>&nbsp;|&nbsp;<a href="http://www.dzwindows.com" target="_blank" title="德州之窗网">德州之窗</a>&nbsp;|&nbsp;<a href="http://bbs.dzwindows.com" target="_blank" title="减河社区">减河社区</a></div>
+	</div>
+	<div class="main">
+		
+	</div>
+	<div class="footer"></div>
+</body>
+</html>
